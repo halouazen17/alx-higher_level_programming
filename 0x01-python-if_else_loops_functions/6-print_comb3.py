95b20a275bb06a0b8e75010fd43e2ef5fcfbb335
halouazen17 <h.alouazen17@outlook.fr>
2023-11-28 14:25:32 -0800

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for a in range(i, 10):
-        if i < a:
-            print("{:d}{:d}".format(i, a),
-                    end="\n" if i == 8 and a == 9 else ", ")
+for i in range(0, 10):
+    for a in range(i + 1, 10):
+        if i == 8 and a == 9:
+            print("{}{}".format(i, a)
+    else:
+                    print("{}{}".format(i, a).end=", ")
