aa2671423ae0f9a156c673117100bc78a206d1dd
halouazen17 <h.alouazen17@outlook.fr>
2023-11-30 12:05:34 -0800

    ?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/pyhton3
 if __name__ == "__main__":
+    """print all names defined by hidden_4 module"""
     import hidden_4
-    for i in dir(hidden_4):
+    names = dir(hidden_4)
+    for i in names:
         if i[:2] != "__":
             print(i)
