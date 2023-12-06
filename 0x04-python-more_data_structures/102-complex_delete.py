f80cbe55cc0b206d60da3a33620433dcb0ee79ee
halouazen17 <h.alouazen17@outlook.fr>
2023-12-06 08:13:42 -0800

    del

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,6 @@
+#!/usr/bin/pyhton3
+def complex_delete(a_dictionary, value):
+    for i in list(a_dictionary.keys()):
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
+    return a_dictionary
